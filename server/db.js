3c935ccdd07aca2f7e1dc3c0fe4018348d8a5b68
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2018-07-31 13:59:48 +0500

    Server config

--- server/db.js
+++ server/db.js
@@ -2,6 +2,6 @@ const mysql = require("mysql")
 const config = require('./config.js')
 
 // live or local
-const db =  mysql.createPool(config.db.local)
+const db =  mysql.createPool(config.db.live)
 
 module.exports = db
