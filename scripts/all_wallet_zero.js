259097ac4596ae183b9d8d4a9bc7a8946c1c8b55
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2018-09-06 04:55:38 +0500

    connection fix

--- scripts/all_wallet_zero.js
+++ scripts/all_wallet_zero.js
@@ -18,41 +18,69 @@ const db = mysql.createPool(config)
 //                 console.log(err)
 //                 return resolve()
 //             } else {
-//                 let throw_error = null
 //                 connection.beginTransaction(async function (err) {
 //                     if (err) {
 //                         connection.release()
 //                         console.log(err)
-//                         return resolve
+//                         return resolve()
 //                     } else {
-//                         connection.query(`
-//                         SELECT m.user_asn_id, m.id, iv.wallet
-//                         FROM info_var_m as iv
-//                         LEFT JOIN members as m
-//                         ON iv.member_id = m.id
-//                         `, async function (err, results) {
-//                                 if (err) {
-//                                     console.log(err)
-//                                     return resolve()
-//                                 } else {
-//                                     if (results.length > 0) {
-//                                         for (result in results) {
-//                                             let mem_wallet = result.wallet
-//                                             let mem_id = result.id
-//                                             let mem_asn_id = result.user_asn_id
-//                                             await new Promise(resolve_2 => {
-//                                                 connection.query('UPDATE info_var_m SET ? WHERE member_id=?', [info_param, id], function (error, results, fields) {
-//                                                     if (error) {
-//                                                         throw_error = error
-//                                                         return resolve_2()
-//                                                     } else { }
+//                         let throw_error = null
+//                         await new Promise(resolve_2 => {
+
+//                             connection.query(`
+//                                 SELECT m.user_asn_id, m.id, iv.wallet
+//                                 FROM info_var_m as iv
+//                                 LEFT JOIN members as m
+//                                 ON iv.member_id = m.id
+//                             `, async function (err, results) {
+//                                     if (err) {
+//                                         throw_error = err
+//                                         return resolve_2()
+//                                     } else {
+//                                         if (results.length > 0) {
+//                                             for (result in results) {
+//                                                 let mem_wallet = result.wallet
+//                                                 let mem_id = result.id
+//                                                 let mem_asn_id = result.user_asn_id
+//                                                 await new Promise(resolve_3 => {
+//                                                     connection.query('UPDATE info_var_m SET ? WHERE member_id=?', [info_param, id], function (error, results, fields) {
+//                                                         if (error) {
+//                                                             throw_error = error
+//                                                             return resolve_3()
+//                                                         } else { }
+//                                                     })
 //                                                 })
-//                                             })
+//                                             }
+//                                             if (throw_error) break
 //                                         }
+//                                         return resolve_2()
 //                                     }
+//                                 })
+
+
+//                         })
+
+//                         if (throw_error) {
+//                             connection.rollback(function () {
+//                                 connection.release()
+//                                 console.log(throw_error)
+//                                 return resolve()
+//                             });
+//                         } else {
+//                             connection.commit(function (err) {
+//                                 if (err) {
+//                                     connection.rollback(function () {
+//                                         connection.release()
+//                                         console.log(err)
+//                                         return resolve()
+//                                     });
+//                                 } else {
+//                                     connection.release()
+//                                     console.log("All Done")
 //                                     return resolve()
 //                                 }
 //                             })
+//                         }
 //                     }
 //                 })
 //             }
