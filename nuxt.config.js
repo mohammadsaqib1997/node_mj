4188541960ee2f5c2f55ad7cb67d92d8b8ad30ab
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2018-10-11 23:44:54 +0500

    www fix error 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,27 +5,47 @@ module.exports = {
   loading: '~/components/loadingComp.vue',
   head: {
     title: 'MJ SUPREME',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Build your Future' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Build your Future'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-      { rel: 'stylesheet', href: '/fontawesome/css/all.min.css' }
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.png'
+      },
+      {
+        rel: 'stylesheet',
+        href: '/fontawesome/css/all.min.css'
+      }
     ],
-    script: [
-      { src: '/js/jquery-3.3.1.min.js' },
-    ]
+    script: [{
+      src: '/js/jquery-3.3.1.min.js'
+    }, ]
   },
-  css : [
+  css: [
     '~/css/main.css'
   ],
   plugins: [
     '~/plugins/s-vue-validator.js',
     '~/plugins/vue-map.js',
-    { src: '~/plugins/route-chg.js', ssr: false },
-    { src: '~/plugins/axios.js', ssr: false }
+    {
+      src: '~/plugins/route-chg.js',
+      ssr: false
+    },
+    {
+      src: '~/plugins/axios.js',
+      ssr: false
+    }
   ],
   modules: [
     '@nuxtjs/axios',
@@ -35,16 +55,29 @@ module.exports = {
     }]
   ],
   axios: {
-    baseURL: (config.dev) ? 'http://127.0.0.1:3000' : 'http://mj-supreme.com'
+    // baseURL: (config.dev) ? 'http://127.0.0.1:3000' : 'http://mj-supreme.com'
+    proxy: true
+  },
+  proxy: {
+    '/api/': {
+      target: (config.dev) ? 'http://127.0.0.1:3000' : 'http://mj-supreme.com',
+      changeOrigin: false,
+      prependPath: false
+    }
   },
   router: {
     scrollBehavior: function (to, from, savedPosition) {
-      return { x: 0, y: 0 }
+      return {
+        x: 0,
+        y: 0
+      }
     }
   },
   build: {
     vendors: ['babel-polyfill'],
-    extend(config, { isClient }) {
+    extend(config, {
+      isClient
+    }) {
       if (!isClient) {
         config.externals.splice(0, 0, function (context, request, callback) {
           if (/^vue2-google-maps($|\/)/.test(request)) {
